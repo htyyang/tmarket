b6a2d7177a0d990b4cc4de3bb7363c01306af751
Haotian Yang <hyang57@u.rochester.edu>
2023-08-11 01:12:16 -0400

    Added wait time

--- tmarket/scrape.py
+++ tmarket/scrape.py
@@ -1,4 +1,9 @@
 from selenium.webdriver import Chrome
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 
 class scrape:
     driver: Chrome = None
+    WAIT_TIMEOUT = 2
+    
