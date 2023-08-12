deee8e789fe69957fadc626dd561fd7de9f37894
Haotian Yang <hyang57@u.rochester.edu>
2023-08-12 18:54:43 -0400

    Added class of error

--- tmarket/scrape.py
+++ tmarket/scrape.py
@@ -1,6 +1,21 @@
+from selenium import webdriver
 from selenium.webdriver import Chrome
 
 class scrape:
     driver: Chrome = None
-    WAIT_TIMEOUT = 2
+    WAIT_TIMEOUT = 1
+    _chrome_options = webdriver.ChromeOptions()
+    _quit = False
+    with open('tmarket/docs/URL.txt', 'r') as file:  # Get the parent domain
+        parent_domain = file.readline().strip()
+    with open('tmarket/docs/USER_AGENT.txt', 'r') as file:
+        user_agent = file.readline().strip()
+
+class error:
+    
+    class URLNavigationError(Exception):
+        pass
+
+    class UnknownDriverError(Exception):
+        pass
     
