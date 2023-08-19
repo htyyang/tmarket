afcee7ba77d3228723db34b6b0f2e9909beea81f
Haotian Yang <hyang57@u.rochester.edu>
2023-08-19 09:57:47 -0400

    Added error handling

--- tmarket/scrape.py
+++ tmarket/scrape.py
@@ -3,11 +3,12 @@ import re
 from selenium import webdriver
 from selenium.webdriver import Chrome
 from .logger_config import get_logger
+from .error import error
 
 class scrape:
     logger = get_logger(__name__)
     driver: Chrome = None
-    WAIT_TIMEOUT = 1
+    _WAIT_TIMEOUT = 0.1
     _chrome_options = webdriver.ChromeOptions()
     _quit = False
     #file_path = os.path.join(current_directory, 'docs/url.txt')
@@ -29,7 +30,7 @@ class scrape:
     def get_current_user_agent(self):
         return self.user_agent
     
-    def setup_parent_domain(self, parent_domain):
+    def set_parent_domain(self, parent_domain):
         pattern = r'^https://www\.transfermarkt\.[a-z]+$'
         if bool(re.match(pattern, parent_domain)):
             print("The default parent domain is: https://www.transfermarkt.com/\n" +
@@ -37,8 +38,9 @@ class scrape:
                 "Create a new player object will still use the default parent domain\n")
             self.logger.info(f"Changed parent domain to {parent_domain}")
             self.parent_domain = parent_domain
+            self.logger.info(f"parent_domain was set to {parent_domain}")
         else:
-            self.logger.warning(f"Failed to changed parent domain to {parent_domain} due to the invalid url")
+            self.logger.exception(f"Failed to changed parent domain to {parent_domain} due to the invalid url", error.URLNavigationError("Invalid parent domain"))
             raise error.URLNavigationError("Invalid parent domain")
             
 
@@ -46,20 +48,25 @@ class scrape:
         return self.parent_domain
 
     def _clear_log_file(self):
-        with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'docs/tmarket.log'),'w') as log_file:
-            pass
+        try:
+            with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'docs/tmarket.log'),'w') as log_file:
+                pass
+        except Exception as e:
+            self.logger.exception("Failed to clear the log file", error.LoggerClearError(e))
+            raise error.LoggerError.LoggerClearError(e)
+        
     def _save_log_copy(self,save_path):
-        save_path = os.path.join(save_path,"logger.log")
-        with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'docs/tmarket.log'), 'r') as original:
-            content = original.read()
-        with open(save_path, 'w') as save:
-            save.write(content)
+        try:
+            if os.path.isdir(save_path):
+                save_path = os.path.join(save_path,"logger.log")
+            elif os.path.isfile(save_path):
+                pass
+            with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'docs/tmarket.log'), 'r') as original:
+                content = original.read()
+            with open(save_path, 'w') as save:
+                save.write(content)
+        except Exception as e:
+            self.logger.exception(f"Failed to save the log file copy to the given path{save_path}", error.LoggerClearError(e))
+            raise error.LoggerError.LoggerSaveError(e)
 
 
-class error:
-    
-    class URLNavigationError(Exception):
-        pass
-
-    class UnknownDriverError(Exception):
-        pass
