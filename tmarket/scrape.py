2fbba173f61aa34ccdcf82e3c349a299c1624c7a
Haotian Yang <hyang57@u.rochester.edu>
2023-08-19 23:20:18 -0400

    Updated

--- tmarket/scrape.py
+++ tmarket/scrape.py
@@ -40,20 +40,14 @@ class scrape:
             self.parent_domain = parent_domain
             self.logger.info(f"parent_domain was set to {parent_domain}")
         else:
-            self.logger.exception(f"Failed to changed parent domain to {parent_domain} due to the invalid url", error.URLNavigationError("Invalid parent domain"))
+            self.logger.exception(f"Failed to changed parent domain to {parent_domain} due to the invalid url: {error.URLNavigationError('Invalid parent domain')}" )
             raise error.URLNavigationError("Invalid parent domain")
             
 
     def get_current_parent_domain(self):
         return self.parent_domain
 
-    def _clear_log_file(self):
-        try:
-            with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'docs/tmarket.log'),'w') as log_file:
-                pass
-        except Exception as e:
-            self.logger.exception("Failed to clear the log file", error.LoggerClearError(e))
-            raise error.LoggerError.LoggerClearError(e)
+   
         
     def _save_log_copy(self,save_path):
         try:
@@ -66,7 +60,7 @@ class scrape:
             with open(save_path, 'w') as save:
                 save.write(content)
         except Exception as e:
-            self.logger.exception(f"Failed to save the log file copy to the given path{save_path}", error.LoggerClearError(e))
+            self.logger.exception(f"Failed to save the log file copy to the given path{save_path}: {error.LoggerError.LoggerSaveError(e)}")
             raise error.LoggerError.LoggerSaveError(e)
 
 
