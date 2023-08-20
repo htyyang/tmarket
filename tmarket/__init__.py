8df4926df291ee603b17f00da70e473a64dae227
Haotian Yang <hyang57@u.rochester.edu>
2023-08-19 23:19:20 -0400

    Updated

--- tmarket/__init__.py
+++ tmarket/__init__.py
@@ -1,6 +1,7 @@
 from .player import Player
+from .logger_config import get_logger, clear_log_file
 
 __author__ = "Haotian Yang"
 __version__ = "0.0.1"
 
-__all__ = ['Player']
+__all__ = ['Player','clear_log_file','get_logger']
